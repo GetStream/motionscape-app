3a05b73ba01ca1883e2d4cf228299382a39147ca
Stefan <stefan.blos@gmail.com>
2022-03-07 18:16:28 +0100

    Initial Commit

---
+++ MacOSExperiments/MacOSExperimentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacOSExperimentsApp.swift
+//  MacOSExperiments
+//
+//  Created by Stefan Blos on 07.03.22.
+//
+
+import SwiftUI
+
+@main
+struct MacOSExperimentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
