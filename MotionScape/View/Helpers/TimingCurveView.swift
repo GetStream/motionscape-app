00bac50d8a6e435e01d78a8812af14564243f327
Stefan <stefan.blos@gmail.com>
2022-03-29 15:49:43 +0200

    Rework UI of TimingCurveView

--- MotionScape/View/Helpers/TimingCurveView.swift
+++ MotionScape/View/Helpers/TimingCurveView.swift
@@ -11,16 +11,37 @@ struct TimingCurveView: View {
     
     var timingCurve: TimingCurve
     
-    let pointRadius: CGFloat = 10
+    let pointRadius: CGFloat = 20
     
     var body: some View {
         GeometryReader { reader in
             ZStack {
+                // Background
                 Path { path in
-                    path.move(to: CGPoint(x: 0, y: reader.size.width))
+                    path.move(to: CGPoint(x: 0, y: reader.size.height / 2))
+                    path.addLine(to: CGPoint(x: reader.size.width, y: reader.size.height / 2))
                     
-                    let firstPoint = createFirstControlPoint(for: timingCurve, in: reader.size.width)
-                    let secondPoint = createSecondControlPoint(for: timingCurve, in: reader.size.width)
+                    path.move(to: CGPoint(x: reader.size.width / 2, y: 0))
+                    path.addLine(to: CGPoint(x: reader.size.width / 2, y: reader.size.height))
+                }
+                .stroke(Color.gray.opacity(0.4), lineWidth: 1)
+                
+                // Control points
+                Path { path in
+                    let firstPoint = createFirstControlPoint(for: timingCurve, in: reader.size)
+                    let secondPoint = createSecondControlPoint(for: timingCurve, in: reader.size)
+                    
+                    path.addEllipse(in: CGRect(x: firstPoint.x - pointRadius / 2, y: firstPoint.y - pointRadius / 2, width: pointRadius, height: pointRadius))
+                    
+                    path.addEllipse(in: CGRect(x: secondPoint.x - pointRadius / 2, y: secondPoint.y - pointRadius / 2, width: pointRadius, height: pointRadius))
+                }
+                .fill(Color.primary)
+                
+                // Curve
+                Path { path in
+                    path.move(to: CGPoint(x: 0, y: reader.size.height))
+                    let firstPoint = createFirstControlPoint(for: timingCurve, in: reader.size)
+                    let secondPoint = createSecondControlPoint(for: timingCurve, in: reader.size)
                     
                     path.addCurve(
                         to: CGPoint(
@@ -30,35 +51,39 @@ struct TimingCurveView: View {
                         control1: firstPoint,
                         control2: secondPoint
                     )
-                    
-                    path.addEllipse(in: CGRect(x: firstPoint.x - pointRadius / 2, y: firstPoint.y - pointRadius / 2, width: pointRadius, height: pointRadius))
-                    
-                    path.addEllipse(in: CGRect(x: secondPoint.x - pointRadius / 2, y: secondPoint.y - pointRadius / 2, width: pointRadius, height: pointRadius))
                 }
-                .stroke(Color.accentColor, lineWidth: 8)
-                .frame(width: reader.size.width, height: reader.size.width, alignment: .center)
+                .stroke(Color.accentColor, lineWidth: 6)
+
             }
             .overlay(Rectangle().stroke(Color.secondary, lineWidth: 2))
+            .clipShape(Rectangle())
         }
     }
     
-    func createFirstControlPoint(for curve: TimingCurve, in size: CGFloat) -> CGPoint {
+    func createFirstControlPoint(for curve: TimingCurve, in size: CGSize) -> CGPoint {
         return CGPoint(
-            x: curve.x0 * size,
-            y: size - curve.y0 * size
+            x: curve.x0 * size.width,
+            y: size.height - curve.y0 * size.height
         )
     }
     
-    func createSecondControlPoint(for curve: TimingCurve, in size: CGFloat) -> CGPoint {
+    func createSecondControlPoint(for curve: TimingCurve, in size: CGSize) -> CGPoint {
         return CGPoint(
-            x: curve.x1 * size,
-            y: size - curve.y1 * size
+            x: curve.x1 * size.width,
+            y: size.height - curve.y1 * size.height
         )
     }
 }
 
 struct TimingCurveView_Previews: PreviewProvider {
     static var previews: some View {
-        TimingCurveView(timingCurve: TimingCurve(x0: 0.17, y0: 0.67, x1: 0.83, y1: 0.67))
+        Group {
+            TimingCurveView(timingCurve: TimingCurve(x0: 0.17, y0: 0.67, x1: 0.83, y1: 0.67))
+                .previewLayout(.fixed(width: 300, height: 300))
+            TimingCurveView(timingCurve: TimingCurve(x0: 0.17, y0: 0.67, x1: 0.83, y1: 0.67))
+                .previewLayout(.fixed(width: 300, height: 200))
+            TimingCurveView(timingCurve: TimingCurve(x0: 0.17, y0: 0.67, x1: 0.83, y1: 0.67))
+                .previewLayout(.fixed(width: 200, height: 300))
+        }
     }
 }
